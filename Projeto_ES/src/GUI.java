c71430216c25d10b75ac89a7de3f4a00f3fa4c83
aplcg-iscteiul <aplcg@iscte-iul.pt>
2019-12-12 18:47:16 +0000

    ooo

--- Projeto_ES/src/GUI.java
+++ Projeto_ES/src/GUI.java
@@ -40,21 +40,15 @@ public class GUI {
 		
 		JPanel panelWest = new JPanel();
 		JPanel panelEast = new JPanel();
-		JPanel panelLM = new JPanel();
 		JPanel panelFE = new JPanel();
 		JPanel panelProcura = new JPanel();
-		JPanel lmButtons = new JPanel();
-		JPanel lmTexts = new JPanel();
 		JPanel feButtons = new JPanel();
 		JPanel feTexts = new JPanel();
 		
 		panelEast.setLayout(new GridLayout(3,1));
 		panelWest.setLayout(new BorderLayout());
-		panelLM.setLayout(new BorderLayout());
 		panelFE.setLayout(new BorderLayout());
 		panelProcura.setLayout(new BorderLayout());
-		lmButtons.setLayout(new BorderLayout());
-		lmTexts.setLayout(new BorderLayout());
 		feButtons.setLayout(new BorderLayout());
 		feTexts.setLayout(new BorderLayout());
 		
@@ -80,18 +74,41 @@ public class GUI {
 		JButton procuraIPlasma = new JButton("Procura IPlasma");
 		JButton procuraPMD = new JButton("Procura PMD");
 		JButton procuraValores = new JButton("Procura Valores do Utilizador");
+		procuraValores.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame frame1 = new JFrame();
+				JPanel panelLM = new JPanel();
+				JPanel lmButtons = new JPanel();
+				JPanel lmTexts = new JPanel();
+				lmButtons.setLayout(new BorderLayout());
+				lmTexts.setLayout(new BorderLayout());
+				
+				panelLM.add(buttonLM, BorderLayout.SOUTH);
+				panelLM.add(lmButtons, BorderLayout.WEST);
+				panelLM.add(lmTexts, BorderLayout.EAST);
+				
+				lmButtons.add(labelLOC, BorderLayout.NORTH);
+				lmButtons.add(labelCYCLO, BorderLayout.SOUTH);
+				lmTexts.add(textLOC, BorderLayout.NORTH);
+				lmTexts.add(textCYCLO, BorderLayout.SOUTH);
+				
+				frame1.add(panelLM);
+				frame1.setVisible(true);
+				frame1.setSize(200, 150);
+				
+			}
+		});
 		
 		frame.add(panelEast,BorderLayout.EAST);
 		frame.add(panelWest, BorderLayout.WEST);
-		panelEast.add(panelLM);
 		panelEast.add(panelProcura);
 		panelEast.add(panelFE);
 		
 		
 		
-		panelLM.add(buttonLM, BorderLayout.SOUTH);
-		panelLM.add(lmButtons, BorderLayout.WEST);
-		panelLM.add(lmTexts, BorderLayout.EAST);
+
 		
 		panelFE.add(buttonFE, BorderLayout.SOUTH);
 		panelFE.add(feButtons, BorderLayout.WEST);
@@ -101,10 +118,7 @@ public class GUI {
 		panelProcura.add(procuraPMD, BorderLayout.CENTER);
 		panelProcura.add(procuraValores, BorderLayout.SOUTH);
 		
-		lmButtons.add(labelLOC, BorderLayout.NORTH);
-		lmButtons.add(labelCYCLO, BorderLayout.SOUTH);
-		lmTexts.add(textLOC, BorderLayout.NORTH);
-		lmTexts.add(textCYCLO, BorderLayout.SOUTH);
+
 		
 		feButtons.add(labelATFD, BorderLayout.NORTH);
 		feButtons.add(labelLAA, BorderLayout.SOUTH);
