b07162cd7ab16582bd68536a355a8690d3ce6b36
Diogo <dmaao11@iscte-iul.pt>
2019-12-12 17:40:55 +0000

    commit

--- Projeto_ES/src/GUI.java
+++ Projeto_ES/src/GUI.java
@@ -34,7 +34,7 @@ public class GUI {
 	
 	public void open() {
 		frame.setVisible(true);
-		frame.setSize(500, 300);
+		
 	}
 	
 	
@@ -67,11 +67,38 @@ public class GUI {
 		JTextField textCYCLO = new JTextField("0");
 		JTextField textATFD = new JTextField("0");
 		JTextField textLAA = new JTextField("0");
-		frame.setLayout(new BorderLayout());
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4,2));
+		JButton buttonLM = new JButton("Definir Long Method");
+		JButton buttonFE = new JButton("Definir Feature envy");
 		
+		frame.add(panelEast,BorderLayout.EAST);
+		frame.add(panelWest, BorderLayout.WEST);
+		panelEast.add(panelLM, BorderLayout.NORTH);
+		panelEast.add(panelFE, BorderLayout.SOUTH);
+		
+		panelLM.add(buttonLM, BorderLayout.SOUTH);
+		panelLM.add(lmButtons, BorderLayout.WEST);
+		panelLM.add(lmTexts, BorderLayout.EAST);
+		
+		panelFE.add(buttonFE, BorderLayout.SOUTH);
+		panelFE.add(feButtons, BorderLayout.WEST);
+		panelFE.add(feTexts, BorderLayout.EAST);
+		
+		lmButtons.add(labelLOC, BorderLayout.NORTH);
+		lmButtons.add(labelCYCLO, BorderLayout.SOUTH);
+		lmTexts.add(textLOC, BorderLayout.NORTH);
+		lmTexts.add(textCYCLO, BorderLayout.SOUTH);
+		
+		feButtons.add(labelATFD, BorderLayout.NORTH);
+		feButtons.add(labelLAA, BorderLayout.SOUTH);
+		feTexts.add(textATFD, BorderLayout.NORTH);
+		feTexts.add(textLAA, BorderLayout.SOUTH);
+		
+//		frame.setLayout(new BorderLayout());
+//		
+//		JPanel panel = new JPanel();
+//		panel.setLayout(new GridLayout(4,2));
+//		
 //		JTextField locLimit = new JTextField();
 //		JTextField cycloLImit = new JTextField();
 		
@@ -125,65 +152,50 @@ public class GUI {
 //		panel.add(cycloLImit);
 		//panel.add(aplicarLimite);
 		
-		frame.add(panel, BorderLayout.WEST);
+//		frame.add(panel, BorderLayout.WEST);
 		
-		JButton buttonLM = new JButton("Definir Long Method");
-		JButton buttonFE = new JButton("Definir Feature envy");
-		JPanel panel4 = new JPanel();
+//		JPanel panel4 = new JPanel();
 		
-		DefaultListModel<String> model = new DefaultListModel<String>();
+//		DefaultListModel<String> model = new DefaultListModel<String>();
 //		JList<String> list = new JList<String>(model);
 		
 		//adicionar a lista qd feita
 		
 //		panel4.add(list);
-		JScrollPane pane = new JScrollPane(panel4);
+//		JScrollPane pane = new JScrollPane(panel4);
 		
-		frame.add(pane, BorderLayout.CENTER);
+//		frame.add(pane, BorderLayout.CENTER);
 		
-		frame.add(panelEast,BorderLayout.EAST);
-		frame.add(panelWest, BorderLayout.WEST);
-		panelEast.add(panelLM, BorderLayout.NORTH);
-		panelEast.add(panelFE, BorderLayout.SOUTH);
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(new GridLayout(1,2));
+//		JPanel panel1 = new JPanel();
+//		panel1.setLayout(new GridLayout(1,2));
 		
-		panelLM.add(buttonLM, BorderLayout.SOUTH);
-		panelLM.add(lmButtons, BorderLayout.WEST);
-		panelLM.add(lmTexts, BorderLayout.EAST);
+//		frame.add(panel1, BorderLayout.SOUTH);
 		
-		panelFE.add(buttonFE, BorderLayout.SOUTH);
-		panelFE.add(feButtons, BorderLayout.WEST);
-		panelFE.add(feTexts, BorderLayout.EAST);
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(new GridLayout(4, 2));
-
-		lmButtons.add(labelLOC, BorderLayout.NORTH);
-		lmButtons.add(labelCYCLO, BorderLayout.SOUTH);
-		lmTexts.add(textLOC, BorderLayout.NORTH);
-		lmTexts.add(textCYCLO, BorderLayout.SOUTH);
-		JTextField t1 = new JTextField("40");
-		JTextField t2 = new JTextField("40");
+//		JPanel panel2 = new JPanel();
+//		panel2.setLayout(new GridLayout(4, 2));
 		
-		JButton loc = new JButton("LOC");
-		loc.addActionListener(new ActionListener() {
+//		JTextField t1 = new JTextField("40");
+//		JTextField t2 = new JTextField("40");
+		
+//		JButton loc = new JButton("LOC");
+//		loc.addActionListener(new ActionListener() {
 			
-			@Override
-			public void actionPerformed(ActionEvent e) {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
 
-				List<String> lista = cond.setLongMethod(t1, t2);
+//				List<String> lista = cond.setLongMethod(t1, t2);
 				
-				for(String s : lista)
-					model.addElement(s);
+//				for(String s : lista)
+//					model.addElement(s);
 				
-				JList<String> list = new JList<String>(model);
-				panel4.add(list);
-			}
-		});
+//				JList<String> list = new JList<String>(model);
+//				panel4.add(list);
+//			}
+//		});
 		
-		JButton cyclo = new JButton("CYCLO");
-		JButton atfd = new JButton("ATFD");
-		JButton laa = new JButton("LAA");
+//		JButton cyclo = new JButton("CYCLO");
+//		JButton atfd = new JButton("ATFD");
+//		JButton laa = new JButton("LAA");
 		
 //		JTextField tx1 = new JTextField();
 		//tx1.setText(" " + cond.getDCI(tx1));
@@ -194,20 +206,20 @@ public class GUI {
 //		JTextField tx4 = new JTextField();
 		//tx4.setText(" " + cond.getADII());
 		
-		panel2.add(loc);
+//		panel2.add(loc);
 //		panel2.add(tx1);
-		panel2.add(cyclo);
+//		panel2.add(cyclo);
 //		panel2.add(tx2);
-		panel2.add(atfd);
+//		panel2.add(atfd);
 //		panel2.add(tx3);
-		panel2.add(laa);
+//		panel2.add(laa);
 //		panel2.add(tx4);
 		
-		frame.add(panel2, BorderLayout.EAST);
+//		frame.add(panel2, BorderLayout.EAST);
 		
 		JPanel panel3 = new JPanel();
 		panel3.setLayout(new GridLayout(1, 4));
-		
+	
 		JButton dci = new JButton("DCI");
 		JButton dii = new JButton("DII");
 		JButton adci = new JButton("ADCI");
@@ -220,18 +232,14 @@ public class GUI {
 		
 		frame.add(panel3, BorderLayout.SOUTH);
 		
-		feButtons.add(labelATFD, BorderLayout.NORTH);
-		feButtons.add(labelLAA, BorderLayout.SOUTH);
-		feTexts.add(textATFD, BorderLayout.NORTH);
-		feTexts.add(textLAA, BorderLayout.SOUTH);
 	}
 	
 	
 	public static void main(String[] args) {
 		GUI frame = new GUI();
-		System.out.println(" agggaas");
+		System.out.println("oi");
 		frame.open();
-		
+		System.out.println("o");
 	}
 
 }
