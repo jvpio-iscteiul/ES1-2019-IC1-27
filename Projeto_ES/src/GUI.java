c4e73d66e735f47fd9172018a70ca056d630eee3
aplcg-iscteiul <aplcg@iscte-iul.pt>
2019-12-12 17:23:41 +0000

    oi

--- Projeto_ES/src/GUI.java
+++ Projeto_ES/src/GUI.java
@@ -14,6 +14,7 @@ public class GUI {
 	
 	public GUI() {
 		frame = new JFrame();
+		frame.setSize(700, 700);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		addFrameContent();
 		frame.pack();
@@ -26,34 +27,59 @@ public class GUI {
 	
 	private void addFrameContent() {
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(4,1));
+		JPanel panelWest = new JPanel();
+		JPanel panelEast = new JPanel();
+		JPanel panelLM = new JPanel();
+		JPanel panelFE = new JPanel();
+		JPanel lmButtons = new JPanel();
+		JPanel lmTexts = new JPanel();
+		JPanel feButtons = new JPanel();
+		JPanel feTexts = new JPanel();
 		
-		JTextField locLimit = new JTextField();
-		JTextField cycloLImit = new JTextField();
-		JButton AplicarLimite = new JButton("Aplicar limites");
-		panel.add(locLimit);
-		panel.add(cycloLImit);
-		panel.add(AplicarLimite);
+		panelEast.setLayout(new BorderLayout());
+		panelWest.setLayout(new BorderLayout());
+		panelLM.setLayout(new BorderLayout());
+		panelFE.setLayout(new BorderLayout());
+		lmButtons.setLayout(new BorderLayout());
+		lmTexts.setLayout(new BorderLayout());
+		feButtons.setLayout(new BorderLayout());
+		feTexts.setLayout(new BorderLayout());
 		
+		JLabel labelLOC = new JLabel("LOC");
+		JLabel labelCYCLO = new JLabel("CYCLO");
+		JLabel labelATFD = new JLabel("ATFD");
+		JLabel labelLAA = new JLabel("LAA");
 		
-		frame.setLayout(new BorderLayout());
-		frame.add(panel, BorderLayout.WEST);
+		JTextField textLOC = new JTextField("0");
+		JTextField textCYCLO = new JTextField("0");
+		JTextField textATFD = new JTextField("0");
+		JTextField textLAA = new JTextField("0");
 		
-		JLabel label = new JLabel();
-		frame.add(label, BorderLayout.CENTER);
+		JButton buttonLM = new JButton("Definir Long Method");
+		JButton buttonFE = new JButton("Definir Feature envy");
 		
-		JPanel panel1 = new JPanel();
-		panel1.setLayout(new GridLayout(1,2));
-		JButton IPlasma = new JButton("IPlasma");
-		JButton PMD = new JButton("PMD");
-		panel1.add(IPlasma);
-		panel1.add(PMD);
+		frame.add(panelEast,BorderLayout.EAST);
+		frame.add(panelWest, BorderLayout.WEST);
+		panelEast.add(panelLM, BorderLayout.NORTH);
+		panelEast.add(panelFE, BorderLayout.SOUTH);
 		
-		frame.add(panel1, BorderLayout.SOUTH);
+		panelLM.add(buttonLM, BorderLayout.SOUTH);
+		panelLM.add(lmButtons, BorderLayout.WEST);
+		panelLM.add(lmTexts, BorderLayout.EAST);
 		
+		panelFE.add(buttonFE, BorderLayout.SOUTH);
+		panelFE.add(feButtons, BorderLayout.WEST);
+		panelFE.add(feTexts, BorderLayout.EAST);
 		
+		lmButtons.add(labelLOC, BorderLayout.NORTH);
+		lmButtons.add(labelCYCLO, BorderLayout.SOUTH);
+		lmTexts.add(textLOC, BorderLayout.NORTH);
+		lmTexts.add(textCYCLO, BorderLayout.SOUTH);
 		
+		feButtons.add(labelATFD, BorderLayout.NORTH);
+		feButtons.add(labelLAA, BorderLayout.SOUTH);
+		feTexts.add(textATFD, BorderLayout.NORTH);
+		feTexts.add(textLAA, BorderLayout.SOUTH);
 	}
 	
 	
